be830ecf66e013e69bd15af26b0ad3b34f5a1cdf
fauzyDev <haliffauzy45@gmail.com>
2025-09-21 01:48:18 +0700

    update: package update

--- src/components/playerTest/PlayerVideo.tsx
+++ src/components/playerTest/PlayerVideo.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
 
-export default function PlayerVideo({ videoId, start }: { videoId: string; start: number }) {
+type PlayerVideo = {
+  videoId: string
+  start: number
+  playerKey?: string
+}
+
+export default function PlayerVideo({ videoId, start, playerKey }: PlayerVideo) {
+  const playerRef = React.useRef<any>(null)
+
   return (
     <div>
       <h2>Test</h2>
       <ReactPlayer
+        ref={playerRef}
+        key={playerKey}
         style={{ width: '100%', height: 'auto', aspectRatio: '16/9' }}
         src={`https://www.youtube.com/watch?v=${videoId}`}
         config={{
@@ -14,6 +24,12 @@ export default function PlayerVideo({ videoId, start }: { videoId: string; start
           },
         }}
         controls
+        onReady={() => {
+          if (playerRef.current) {
+            console.log("SeekTo", start)
+            playerRef.current.seekTo(start, 'seconds')
+          }
+        }}
       />
     </div>
   )
