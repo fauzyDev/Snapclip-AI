132283e938d26ef3a0b432ee199ac1096020a5b1
fauzyDev <haliffauzy45@gmail.com>
2025-10-01 00:53:30 +0700

    update: package update

--- src/components/playerTest/PlayerVideo.tsx
+++ src/components/playerTest/PlayerVideo.tsx
@@ -7,21 +7,36 @@ type PlayerVideo = {
 }
 
 export default function PlayerVideo({ videoId, start }: PlayerVideo) {
-  const playerRef = React.useRef<any>(null);
+  // const playerRef = React.useRef<any>(null);
+  // console.log("🎥 Player render:", { videoId, start });
+
+  // React.useEffect(() => {
+  //   if (playerRef.current && start !== undefined) {
+  //     const timer = setTimeout(() => {
+  //       playerRef.current?.seekTo(start, "seconds");
+  //     }, 500);
+  //     return () => clearTimeout(timer);
+  //   }
+  // }, [start, videoId]);
+
+  console.log("🎥 [Render] videoId:", videoId, "start:", start);
 
   React.useEffect(() => {
-    if (playerRef.current && start !== undefined) {
-      const timer = setTimeout(() => {
-        // true biar selalu lompat walau out of buffer
-        playerRef.current?.seekTo(start, "seconds");
-      }, 500);
-      return () => clearTimeout(timer);
-    }
-  }, [start, videoId]);
+    console.log("✅ [Mounted] Player dengan props:", { videoId, start });
+    return () => {
+      console.log("❌ [Unmounted] Player dengan props:", { videoId, start });
+    };
+  }, [videoId, start]);
+
+  // jangan render kalau datanya belum siap
+  if (!videoId || start === undefined) {
+    console.log("⏳ Player belum render, data belum siap...");
+    return null;
+  }
 
   return (
     <ReactPlayer
-      ref={playerRef as any}
+      // ref={playerRef as any}
       style={{ width: '100%', height: 'auto', aspectRatio: '16/9' }}
       src={`https://www.youtube.com/watch?v=${videoId}`}
       config={{
